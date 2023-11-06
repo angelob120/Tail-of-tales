e660388a5dc81ca3cc78c134128ccea664fa69de
Nychelle <90415090@thedps.org>
2023-11-06 16:00:56 -0500

    I put in some arrays

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -3,32 +3,56 @@
 //  CLIStoryTemplate
 //
 
+//  FixingMyStory
+//
+//  Created by Nychelle Gaines on 11/6/23.
+//
+
 import Foundation
 
-func chapterOne() {
-    // Your portion of the story goes here
-    print("Tail of The Tailless Squirrel")
-    var protagonistName = "Damian"
-  
-    class in_The_Woods{
-        var protagonistName = "Damian"
-        var hasTail: Bool
-        
-        func start() -> String {
-            return "Once upon a time, in a dark, sun-dappled forest, there lived a squirrel named \(protagonistName).\(protagonistName) was not your typical squirrel; he was tremendously self-centered. His lush, bushy tail was on display as he pranced around the treetops for the enjoyment of any nearby forest animal. He thought that the woodland's most magnificent feature was his tail. He was certain that he was the most daring and charming squirrel in the forest, and he used every opportunity to prove it."
-        }
-    }
-  
-  let story1 =   "Once upon a time, in a dark, sun-dappled forest, there lived a squirrel named Damian. Damian was not your typical squirrel; he was tremendously self-centered. His lush, bushy tail was on display as he pranced around the treetops for the enjoyment of any nearby forest animal. He thought that the woodland's most magnificent feature was his tail. He was certain that he was the most daring and charming squirrel in the forest, and he used every opportunity to prove it."
-
-  let story2 =  "Damian was hopping from branch to limb one bright, beautiful morning, bragging about his tail. Damian couldn't resist the opportunity to flaunt his tail. Of course, it is! It's a tail that makes every squirrel envious. Nobody in this forest has a tail like mine!The berries there are the juiciest and most delicious you'll ever taste. I'd love to show you, but it's a bit of a trek, and I'm worried about my own tail. Would you be willing to accompany me and keep an eye out for any danger? I'm sure a squirrel as courageous as you won't have any trouble."
-    
-   let story3 = "As he returned to the squirrel colony, he expected comfort and support from his fellow squirrels. However, what he encountered was quite the opposite."
-
-   let story4 = "To his shock and dismay, some of the squirrels within the colony began to ridicule and isolate him. They cruelly called him ugly and imperfect. In their eyes, without his glorious tail, Damian was no longer the handsome squirrel he once was. His appearance had been his defining feature, and with it gone, he was scorned."
-
-   let story5 = "The leader of the colony, a stern and unkind squirrel named Reggie, made the decision to exclude Damian from their tight-knit community. He believed that the colony should only consist of the most beautiful and perfect squirrels, and he considered Damian's current appearance unacceptable."
-
-   let story6 = "Heartbroken and humiliated, Damian was cast out from the only family he had ever known. He was left to fend for himself in the vast forest, where he felt isolated and alone."
-    
+struct Character {
+    var name: String
+    var tailSize: String
+    var description: String
 }
+   
+var damian = Character(name: "Damian", tailSize: "big and luxurious", description: "a courageous squirrel with a magnificent tail")
+
+var squirrellettes: [Character] = [squirrellette1, squirrellette2, squirrellette3]
+
+let squirrellette1 = Character(name: "sally", tailSize: "average", description: "average")
+let squirrellette2 = Character(name: "lilly", tailSize: "average", description: "average")
+let squirrellette3 = Character(name: "josie", tailSize: "average", description: "average")
+
+let forage = ["berries", "nuts", "seeds"]
+
+//In a loop is gonna be him foraging for the forage array, in the lop will be him getting each iteam
+    
+
+
+//******************************************************************************************
+
+func chapterOne() {
+    print("Once upon a time, in a dark, sun-dappled forest, there lived a squirrel named \(damian.name) \(damian.name) was not your typical squirrel; \(damian.description)")
+
+}
+chapterOne()
+
+//+Variables and constants, Conditionals, +Functions, +Arrays, Loops, +Structs, Enums, Optionals
+
+//  "Once upon a time, in a dark, sun-dappled forest, there lived a squirrel named Damian. Damian was not your typical squirrel; he was tremendously self-centered. His lush, bushy tail was on display as he pranced around the treetops for the enjoyment of any nearby forest animal. He thought that the woodland's most magnificent feature was his tail. He was certain that he was the most daring and charming squirrel in the forest, and he used every opportunity to prove it."
+
+ // "Damian was hopping from branch to limb one bright, beautiful morning, bragging about his tail. Damian couldn't resist the opportunity to flaunt his tail. A group of squirrellets came up to him giggling and fauning over his good looks, Damian your tail is so big and luxurious. Of course, it is! It's a tail that makes every squirrel envious. Nobody in this forest has a tail like mine! We are going to a opening patch deep in the forest near the edge of the foxes territory one of the girls said. The berries there are the juiciest and most delicious you'll ever taste. I'd love to show you, but it's a bit of a trek, and I'm worried about the foxes. Would you be willing to accompany us and keep an eye out for any danger? I'm sure a squirrel as courageous as you won't have any trouble." (Array, Conditional, loop)
+
+ //let story3 = "Sure a fox never scared me just lead the way."
+
+//let story5 = ugiryutgjk
+//
+//   let story6 = "As he returned to the squirrel colony, he expected comfort and support from his fellow squirrels. However, what he encountered was quite the opposite."(function)
+//
+//   let story7 = "To his shock and dismay, some of the squirrels within the colony began to ridicule and isolate him. They cruelly called him ugly and imperfect. In their eyes, without his glorious tail, Damian was no longer the handsome squirrel he once was. His appearance had been his defining feature, and with it gone, he was scorned."
+//
+//   let story8 = "The leader of the colony, a stern and unkind squirrel named Reggie, made the decision to exclude Damian from their tight-knit community. He believed that the colony should only consist of the most beautiful and perfect squirrels, and he considered Damian's current appearance unacceptable."
+//
+//   let story9 = "Heartbroken and humiliated, Damian was cast out from the only family he had ever known. He was left to fend for himself in the vast forest, where he felt isolated and alone."
+//
