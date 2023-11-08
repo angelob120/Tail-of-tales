901a1c895eef2ac9aaf59ee5ed9e3f1d59a9a590
Angelo Brown <130711987+angelob120@users.noreply.github.com>
2023-11-08 13:23:07 -0500

    Update Chapter1.swift

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -1,141 +1,82 @@
-//
-//  Chapter1.swift
-//  CLIStoryTemplate
-//
-
 import Foundation
 
-// Enums to represent different areas within the forest.
-enum ForestArea {
-    case edge // The outer perimeter of the forest.
-    case deepForest // The dense, central part of the forest.
-    case colony // The area where the squirrels live.
-}
-
-// Structs to define the attributes of characters in the story.
 struct Character {
-    var name: String // The character's name.
-    var tailSize: String // Descriptive size of the tail.
-    var description: String // A brief description of the character.
-    var isTailIntact: Bool // Boolean to indicate if the character's tail is intact.
+    var name: String
+    var tailSize: String
+    var description: String
 }
+   
+var damian = Character(name: "Damian", tailSize: "big and luxurious", description: "a courageous squirrel with a magnificent tail")
 
-// Function to run the first chapter of the story.
+var squirrellettes: [Character] = [squirrellette1, squirrellette2, squirrellette3]
+
+let squirrellette1 = Character(name: "sally", tailSize: "average", description: "average")
+let squirrellette2 = Character(name: "lilly", tailSize: "average", description: "average")
+let squirrellette3 = Character(name: "josie", tailSize: "average", description: "average")
+
+var didGoToForest : Bool = false
+var hasTail : Bool = true
+
+var foodType = food.berries
+
+var returned : Bool = true
+
+
+
+//make a loop for berry picking
+
+
+
+
+
+
+    
+
+
+//*******************************************************//
+
 func chapterOne() {
-    // Define the main character, kingDamian, as an optional in case he needs to be removed from the story later.
-    var kingDamian: Character? = Character(name: "Damian", tailSize: "big and luxurious", description: "a courageous squirrel king with a magnificent tail", isTailIntact: true)
+    print("Once upon a time, in a dark, sun-dappled forest, there lived a squirrel named \(damian.name) \(damian.name) was not your typical squirrel; \(damian.description)")
     
-    // Arrays to store other characters and items to be foraged.
-    let squirrellettes = [ // Supporting characters in the story.
-        Character(name: "Sally", tailSize: "average", description: "average", isTailIntact: true),
-        Character(name: "Lilly", tailSize: "average", description: "average", isTailIntact: true),
-        Character(name: "Josie", tailSize: "average", description: "average", isTailIntact: true)
-    ]
+    print("Damian was invited by the squirrellettes to go find food so he followed them to the patch in the forest close to the foxes territory.")
+    print("Their agile paws and sharp eyes scouring the terrain, they picked ripe \(food.berries) from bushes, searched the ground for dropped nuts, and unearthed seeds from the rich soil.")
+    print("As they diligently filled their cheek pouches and reveled in nature's abundance.A sly fox with acute instincts stepped out of the undergrowth and eyed the squirrel gathering with a menacing look.")
+    didGoToForest = true
+    goToForest()
+    returned = true
+    returnFromForest()
+    
 
-    let forage = ["berries", "nuts", "seeds"] // Items that the characters will forage for.
-    
-    // Print the opening scene of the story using optional chaining and nil-coalescing operator.
-    print("Once upon a time, in a dark, sun-dappled forest, there lived a squirrel named \(kingDamian?.name ?? "The King"). \(kingDamian?.name ?? "He") was not your typical squirrel; \(kingDamian?.description ?? "")")
+ 
+    }
 
-    // A loop to describe the foraging activities of the main character.
-    for item in forage {
-        print("\(kingDamian?.name ?? "The King") forages for \(item).")
-    }
-    
-    // Conditional to simulate an encounter with a fox and its consequences.
-    if kingDamian?.isTailIntact == true {
-        print("\(kingDamian?.name ?? "The King") encountered a fox!")
-        kingDamian?.isTailIntact = false // The tail is no longer intact.
-        kingDamian?.tailSize = "none" // Update the tail size to reflect the loss.
-        print("Oh no! The fox has bitten off \(kingDamian?.name ?? "The King")'s tail!")
-    }
-    
-    // Express the main character's grief over the loss of his tail.
-    if !(kingDamian?.isTailIntact ?? true) {
-        print("\(kingDamian?.name ?? "The King") mourns the loss of his beautiful tail, once the pride of the land.")
-    }
-    
-    // Optional binding to safely unwrap the kingDamian variable and continue the story.
-    if let damian = kingDamian {
-        print("Despite the loss, \(damian.name) remains determined to lead the squirrellettes safely through the forest.")
-    } else {
-        print("The story cannot continue without our king.") // If kingDamian is nil, the story cannot proceed.
+func goToForest() {
+    if didGoToForest == false {
+        hasTail = true
+        print("He escapes the tip of his tail grazing the foxes jaws.")
+    }else {
+        hasTail = false
+        print("He was cornered and when escaping he unfortunately loses his tail.")
     }
 }
 
-// The function chapterOne() is designed to be called from a suitable point in the application to start the story.
-// It currently remains commented out to prevent execution outside of the narrative context.
+enum food {
+case berries,nuts, seeds
+}
+let Food = food.berries
 
 
-
-
-
-
-
-//
-//  Chapter1.swift
-//  CLIStoryTemplate
-//
-
-//  FixingMyStory
-//
-//  Created by Nychelle Gaines on 11/6/23.
-//
-
-//import Foundation
-//
-//struct Character {
-//    var name: String
-//    var tailSize: String
-//    var description: String
-//}
-//
-//var damian = Character(name: "Damian", tailSize: "big and luxurious", description: "a courageous squirrel with a magnificent tail")
-//
-//var squirrellettes: [Character] = [squirrellette1, squirrellette2, squirrellette3]
-//
-//let squirrellette1 = Character(name: "sally", tailSize: "average", description: "average")
-//let squirrellette2 = Character(name: "lilly", tailSize: "average", description: "average")
-//let squirrellette3 = Character(name: "josie", tailSize: "average", description: "average")
-//
-//let forage = ["berries", "nuts", "seeds"]
-
-//In a loop is gonna be him foraging for the forage array, in the lop will be him getting each iteam
+func returnFromForest() {
+    if returned == true{
+        print("As he returned to the squirrel colony, he expected comfort and support from his fellow squirrels. However, what he encountered was quite the opposite.")
+    }else{
+        returned = false
+        print("He died")
+      
     
-
-
-//******************************************************************************************
-
-//func chapterOne() {
-//    print("Once upon a time, in a dark, sun-dappled forest, there lived a squirrel named \(damian.name) \(damian.name) was not your typical squirrel; \(damian.description)")
-//
-//}
-//chapterOne()
-
-//+Variables and constants, Conditionals, +Functions, +Arrays, Loops, +Structs, Enums, Optionals
-
-//  "Once upon a time, in a dark, sun-dappled forest, there lived a squirrel named Damian. Damian was not your typical squirrel; he was tremendously self-centered. His lush, bushy tail was on display as he pranced around the treetops for the enjoyment of any nearby forest animal. He thought that the woodland's most magnificent feature was his tail. He was certain that he was the most daring and charming squirrel in the forest, and he used every opportunity to prove it."
-
- // "Damian was hopping from branch to limb one bright, beautiful morning, bragging about his tail. Damian couldn't resist the opportunity to flaunt his tail. A group of squirrellets came up to him giggling and fauning over his good looks, Damian your tail is so big and luxurious. Of course, it is! It's a tail that makes every squirrel envious. Nobody in this forest has a tail like mine! We are going to a opening patch deep in the forest near the edge of the foxes territory one of the girls said. The berries there are the juiciest and most delicious you'll ever taste. I'd love to show you, but it's a bit of a trek, and I'm worried about the foxes. Would you be willing to accompany us and keep an eye out for any danger? I'm sure a squirrel as courageous as you won't have any trouble." (Array, Conditional, loop)
-
- //let story3 = "Sure a fox never scared me just lead the way."
-
-//let story5 = ugiryutgjk
-//
-//   let story6 = "As he returned to the squirrel colony, he expected comfort and support from his fellow squirrels. However, what he encountered was quite the opposite."(function)
-//
-//   let story7 = "To his shock and dismay, some of the squirrels within the colony began to ridicule and isolate him. They cruelly called him ugly and imperfect. In their eyes, without his glorious tail, Damian was no longer the handsome squirrel he once was. His appearance had been his defining feature, and with it gone, he was scorned."
-//
-//   let story8 = "The leader of the colony, a stern and unkind squirrel named Reggie, made the decision to exclude Damian from their tight-knit community. He believed that the colony should only consist of the most beautiful and perfect squirrels, and he considered Damian's current appearance unacceptable."
-//
-//   let story9 = "Heartbroken and humiliated, Damian was cast out from the only family he had ever known. He was left to fend for himself in the vast forest, where he felt isolated and alone."
-//
-
-
-
-
-
-
+    }
+}
+     
 
 
 
